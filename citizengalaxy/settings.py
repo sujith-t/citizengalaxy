9feb78fc141f79a0330174f9c9a2ebfd1a48e8db
SujithThillymplam <Sujith.Thillymplam@cloudsolutions.com.sa>
2024-02-12 21:26:03 +0530

    adding page layout and icons

--- citizengalaxy/settings.py
+++ citizengalaxy/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -51,10 +51,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "citizengalaxy.urls"
 
+# static folder to keep all html static assets
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
