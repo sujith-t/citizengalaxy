2a74d63e500394f18929e0cc9136ba34c6ef0922
SujithThillymplam <Sujith.Thillymplam@cloudsolutions.com.sa>
2024-02-23 23:46:53 +0530

    detecting and adjusting outlier for the test data logic is performed

--- api/service/classifier.py
+++ api/service/classifier.py
@@ -1,8 +1,8 @@
 from citizengalaxy.settings import STATICFILES_DIRS
-#from sklearn.ensemble import RandomForestClassifier
+
 import joblib
 import pandas as pd
-from sklearn.impute import KNNImputer
+
 
 # @author Sujith T
 class GalaxyClassClassifier:
@@ -11,40 +11,78 @@ class GalaxyClassClassifier:
         dir_path = "/galaxy_classification/dataset"
         model_path = dir_path + "/rf_model.joblib"
         self._classifier = joblib.load(model_path)
-        self._df = pd.read_csv(dir_path + "/categorized.csv")
-        print("Model and csv data loaded")
+        self._min_max_df = pd.read_csv(dir_path + "/feature_min_max.csv")
+        self._features = []
+        for i in range(self._min_max_df.shape[0]):
+            self._features.append(self._min_max_df["feature"][i])
 
-        self._replace_missing_values()
-        print("Missing values are imputed for csv data")
+        print("Model and feature min/max are loaded")
 
-        self._adjust_outliers_by_groups()
-        print("Outliers are adjusted for csv data")
+    def _detect_adjust_border_values(self, data={}, border_cutoff_percent=10):
+        # converting the keys to uppercase
+        data = {k.upper(): data[k] for k in data}
 
-    def _replace_missing_values(self):
-        imputer = KNNImputer(n_neighbors=5)
-        columns_for_knn_impute = ["PETROMAG_MU"]
-        imputed_data = imputer.fit_transform(self._df[columns_for_knn_impute])
-        tmp_df = pd.DataFrame(imputed_data, columns=columns_for_knn_impute)
+        inx = 0
+        # locate the row relavant for a give feature
+        for col in self._features:
+            # first detect the direction for the feature min = -1, max = 1
+            adjusted_feature_value = data[col]
 
-        self._df["PETROMAG_MU"] = tmp_df["PETROMAG_MU"]
+            tmp_df = self._min_max_df[self._min_max_df["feature"] == col]
+            max_lookup = (tmp_df['max_order'][inx]).split()
+            min_lookup = (tmp_df['min_order'][inx]).split()
 
-    def _adjust_outliers_by_groups(self):
-        omit_columns = ["OBJ_ID", "REGION", "code"]
-        distinct = set(self._df["code"].unique())
+            # collect first 2 applicable classes and check whether any outlier handling required
+            applicable_class = []
 
-        for v in distinct:
-            tmp_df = self._df[self._df["code"] == v]
-            tmp_df = tmp_df.drop(columns=omit_columns)
-            q1 = tmp_df.quantile(0.25)
-            q3 = tmp_df.quantile(0.75)
-            irq = q3 - q1
-            lower_limit = q1 - (1.5 * irq)
-            upper_limit = q3 + (1.5 * irq)
+            # detect the applicable range starting with smallest max value per class
+            for class_max_col in max_lookup:
+                cls = class_max_col.split("_")[0]
+                if tmp_df[class_max_col][inx] > data[col] > tmp_df[(cls + "_min")][inx]:
+                    applicable_class.append(cls)
 
-            # adjust the outlier value for each column
-            for col in tmp_df.columns:
-                tmp_df.loc[tmp_df[col] < lower_limit[col], col] = lower_limit[col]
-                tmp_df.loc[tmp_df[col] > upper_limit[col], col] = upper_limit[col]
-                self._df.loc[tmp_df.index, col] = tmp_df[col]
-    def predict_galaxy_class(self, features = []):
-        return self._classifier.predict(features)
+                    if len(applicable_class) >= 2:
+                        break
+
+            # detecting whether the feature specific data point is moving towards the max|min direction from it's
+            # range midpoint
+            if len(applicable_class) > 0:
+                cls_half_range_distance = (tmp_df[applicable_class[0] + "_max"][inx] -
+                                           tmp_df[applicable_class[0] + "_min"][inx]) / 2
+                cls_range_mid_point = tmp_df[applicable_class[0] + "_max"][inx] - cls_half_range_distance
+                distance_from_middle = data[col] - cls_range_mid_point
+                cutoff_distance = (100 - border_cutoff_percent) / 100 * cls_half_range_distance
+
+                # towards max
+                if distance_from_middle > 0:
+                    cut_off_point = cls_range_mid_point + cutoff_distance
+                    # is it nearing the max value
+                    if data[col] > cut_off_point:
+                        print(col, "more than max cutoff")
+                        adjusted_feature_value = tmp_df[applicable_class[0] + "_max"][inx]
+
+                else:  # towards min
+                    cut_off_point = cls_range_mid_point - cutoff_distance
+                    # is it nearing the min value
+                    if data[col] < cut_off_point:
+                        print(col, "less than min cutoff")
+                        adjusted_feature_value = tmp_df[applicable_class[0] + "_min"][inx]
+
+            # detecting whether the datapoint of a feature is beyond the highest max class of a feature
+            if len(applicable_class) == 0 and data[col] > tmp_df[max_lookup[-1]][inx]:
+                adjusted_feature_value = tmp_df[max_lookup[-1]][inx]
+                print(col, "max outlier")
+
+            # detecting whether the datapoint of a feature is beyond the lowest min class of a feature
+            if len(applicable_class) == 0 and data[col] < tmp_df[min_lookup[-1]][inx]:
+                adjusted_feature_value = tmp_df[min_lookup[-1]][inx]
+                print(col, "min outlier")
+
+            data[col] = adjusted_feature_value
+            inx += 1
+
+        return data
+
+    def predict_galaxy_class(self, data={}):
+        data = self._detect_adjust_border_values(data)
+        # return self._classifier.predict(features)
--- api/views.py
+++ api/views.py
@@ -6,14 +6,14 @@ from .service.classifier import GalaxyClassClassifier
 # @author Sujith T
 # In God We Trust 2024
 
-model = GalaxyClassClassifier()
+classifier = GalaxyClassClassifier()
 
 @api_view(["POST"])
 def classify_galaxy(request):
     serializer = ClassifySerializer(data=request.data)
 
     if serializer.is_valid():
-        model.predict_galaxy_class([])
+        classifier.predict_galaxy_class(serializer.data)
         return Response("Sa", status=200)
     else:
         return Response({"message": "Invalid data posted", "status": 400, "errors": serializer.errors})
