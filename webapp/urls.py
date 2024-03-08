c395f3399f328b314f17dd1a81e0f3e2c9d811e7
SujithThillymplam <Sujith.Thillymplam@cloudsolutions.com.sa>
2024-03-08 23:24:32 +0530

    Building the classifier UI

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('statistics', views.statistics, name='statistics')
+    path('statistics', views.statistics, name='statistics'),
+    path('classify', views.classify, name='classify')
 ]
