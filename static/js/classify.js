a18a67cf9e3c6cb9c1e975c6c5a63ff409f0709e
SujithThillymplam <Sujith.Thillymplam@cloudsolutions.com.sa>
2024-03-12 23:22:47 +0530

    binding classification UI with the backend and styling

--- static/js/classify.js
+++ static/js/classify.js
@@ -1,17 +1,23 @@
+/**
+ * @author Sujith T
+ * Deus et Scientia Erit Pactum Meum 2024
+ **/
 $(document).ready(function(){
 
     let clzImgSelector = ".final-label img";
     let clzTextLabel = ".cls-label";
+    let payload = {};
 
     $(".btn-grad").click(function(){
         //build the post payload first, validate all having numeric values
-        var payload = {};
         $(".tbl-classify .numeric").each(function(inx) {
             let userVal = parseFloat($(this).val());
             if(isNaN(userVal)) {
                 alert("Enter a valid numeric value for " + $(this).attr("id"));
-                return;
+                return false;
             }
+
+            payload[$(this).attr("id")] = userVal;
         });
 
         //immediately show processing image
@@ -22,50 +28,51 @@ $(document).ready(function(){
 
         //we make it sleep for 5 min to make user feel the data is processing
         setTimeout(callAPIToClassify, 5000);
-
-        //post it to API
-        /*$.ajax({
-            type: "POST",
-            url: "/api/v1/galaxy/classify",
-            data: payload,
-            async: false,
-            success: function (response) {
-                alert(response)
-            }
-        })*/
-
     });
 
     function callAPIToClassify() {
-        //these are class E styles
-        var clzGradient = "#bfe9f5, #74dbf7";
-        var clzTextColor = "#059eeb";
-
-        // SB class types
-        clzGradient = "#f7dae4, #d46c7d, #f7dae4";
-        clzTextColor = "#f00c58";
-
-        //S Types
-        clzGradient = "#b7ffb0, #96f78d";
-        clzTextColor = "#16e002";
+        //class E type default style
+        let clzGradient = "#bfe9f5, #74dbf7";
+        let clzTextColor = "#059eeb";
 
         $(clzImgSelector).hide();
 
-        $(".final-label").css("background-image", 'linear-gradient(#bfe9f5, #74dbf7)');
         //post it to API
         $.ajax({
             type: "POST",
             url: "/api/v1/galaxy/classify",
             data: payload,
             async: false,
-            success: function (response) {
+            success: function (data, status, jqXHR) {
+                //when successful response received
+                if (jqXHR.status === 200) {
+                    let clazz = data[0][0];
 
+                    //if SB types
+                    if(clazz.search(/SB[a-d]/) > -1) {
+                        clzGradient = "#f7dae4, #d46c7d, #f7dae4";
+                        clzTextColor = "#f00c58";
+                    }
+
+                    //if S types
+                    if(clazz.search(/S[a-e]/) > -1) {
+                        clzGradient = "#b7ffb0, #96f78d, #b7ffb0";
+                        clzTextColor = "#16e002";
+                    }
+
+                    $(".final-label").css("background-image", "linear-gradient(" + clzGradient + ")");
+                    $(clzTextLabel).text(clazz);
+                    $(clzTextLabel).css("color", clzTextColor);
+                    $(clzTextLabel).show();
+                }
+            },
+            fail: function (response) {
+                console.log("Failure....");
                 console.log(response);
             }
         });
 
-        $(clzTextLabel).text("Sba");
-        $(clzTextLabel).css("color", "#059eeb");
-        $(clzTextLabel).show();
+        payload = {};
     }
+
 });
