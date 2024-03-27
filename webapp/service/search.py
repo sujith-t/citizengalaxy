5261bdcfb841e1beea1f8b2e3ff28f508e58ea9a
SujithThillymplam <Sujith.Thillymplam@cloudsolutions.com.sa>
2024-03-27 22:07:25 +0530

    Initial catalog display setup and querying logic to the database

---
+++ webapp/service/search.py
@@ -0,0 +1,22 @@
+from django.core.paginator import Paginator
+from webapp.models import SdssMetadataModel
+
+
+# @author Sujith T
+# Deus et Scientia Erit Pactum Meum 2024
+class GalaxyLocatorServiceImpl:
+
+    default_page_size = 25
+
+    def search(self, param: dict):
+        pass
+
+    def get_page_result(self, page_no):
+        query_sets = SdssMetadataModel.objects.select_related("catalog")
+        paginator = Paginator(query_sets, self.default_page_size)
+        page = paginator.page(page_no)
+
+        return page.object_list
+
+    def get_details(self, obj_id):
+        pass
