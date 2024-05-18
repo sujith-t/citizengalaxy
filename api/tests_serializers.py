2e8401b6c2f0c07662dfb47362f56d423753c040
SujithThillymplam <Sujith.Thillymplam@cloudsolutions.com.sa>
2024-05-18 18:42:22 +0530

    Adding unit-tests

---
+++ api/tests_serializers.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class SerializerTestCase(unittest.TestCase):
+
+    def test_isvalid_for_petro50(self):
+        self.assertEqual(True, True)  # add assertion here
+
+    def test_isvalid_for_petro90(self):
+        self.assertEqual(True, True)  # add assertion here
+
+    def test_required_redshift(self):
+        self.assertEqual(True, True)  # add assertion here
+
+    def test_min_redshift(self):
+        self.assertEqual(True, True)  # add assertion here
+
+    def test_max_redshift(self):
+        self.assertEqual(True, True)  # add assertion here
