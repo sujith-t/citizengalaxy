c6c3d306416dc1c5de13c3f5be0bd8516452bddb
SujithThillymplam <Sujith.Thillymplam@cloudsolutions.com.sa>
2024-06-27 16:45:02 +0530

    Integrating charts to display statistics

---
+++ static/js/stats.js
@@ -0,0 +1,63 @@
+/**
+ * @author Sujith T
+ * Deus et Scientia Erit Pactum Meum 2024
+ **/
+$(document).ready(function(){
+
+    new Chart($('#groups'), {
+        type: 'pie',
+        data: {
+            labels: ['Ecliptic', 'Spiral','Barred Spiral'],
+            datasets: [{
+                data: [10, 20, 30],
+                //backgroundColor: ["Yellow", "Blue", "Green"]
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Galaxy Types Distribution'
+                }
+            }
+        }
+    });
+
+    new Chart($('#clazz'), {
+        type: 'bar',
+        data: {
+            labels: [1,2,3,4,5,6,7],
+            datasets: [{
+                label: "hello",
+                data: [65, 59, 80, 81, 56, 55, 40],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(201, 203, 207, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                    'rgb(153, 102, 255)',
+                    'rgb(201, 203, 207)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Galaxy Types Distribution'
+                }
+            }
+        }
+    });
+});
