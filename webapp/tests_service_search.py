3830ed070f838e57fccabd3acf6c12a2613bc18b
SujithThillymplam <Sujith.Thillymplam@cloudsolutions.com.sa>
2024-07-29 16:03:11 +0530

    unit test for testing pagination

--- webapp/tests_service_search.py
+++ webapp/tests_service_search.py
@@ -1,13 +1,24 @@
 import unittest
 
+from webapp.service.search import GalaxyLocatorServiceImpl
+
 
 class GalaxyLocatorTestCase(unittest.TestCase):
 
     def test_instance(self):
-        self.assertEqual(True, True)  # add assertion here
+        locator = GalaxyLocatorServiceImpl()
+        self.assertTrue(isinstance(locator, GalaxyLocatorServiceImpl))  # add assertion here
 
     def test_paginated_results(self):
-        self.assertEqual(True, True)  # add assertion here
+        locator = GalaxyLocatorServiceImpl()
+        # checking whether the page size is 25
+        default_page_size = 25
+        no_of_pages, objects = locator.get_page_result(3)
+        self.assertEqual(default_page_size, objects.count())
+
+        # when page number goes beyond the total pages no objects will be returned
+        no_of_pages, objects = locator.get_page_result(no_of_pages + 1)
+        self.assertEqual(0, len(objects))
 
     def test_search_by_object_id(self):
         self.assertEqual(True, True)  # add assertion here
